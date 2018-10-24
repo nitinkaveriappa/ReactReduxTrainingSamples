8da4c8bfc30dea475a89f95e3a1a89981119db49
Nitin Kaveriappa Udiyanda Muthanna <nitinkaveriappa@gmail.com>
2018-10-24 18:12:00 +0530

    Test

--- ReduxSimpleStarter-master/src/components/app.js
+++ ReduxSimpleStarter-master/src/components/app.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import NavBar from './navbar';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <NavBar />
     );
   }
 }
