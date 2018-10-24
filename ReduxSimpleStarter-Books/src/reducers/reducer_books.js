041d39c81ea27b5dac40287acf769ead19a50a8e
Nitin Kaveriappa Udiyanda Muthanna <nitinkaveriappa@gmail.com>
2018-10-24 13:48:09 +0530

    .

---
+++ ReduxSimpleStarter-Books/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'JavaScript', pages: 101 },
+        { title: 'Harry Potter', pages: 91 },
+        { title: 'Eloquent Ruby', pages: 81 },
+        { title: 'The Dark Tower', pages: 1 }
+    ];
+}
