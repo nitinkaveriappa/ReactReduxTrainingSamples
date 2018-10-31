1fbedf70d1b350b9eb70647123fe5bddd22b73e8
Nitin Kaveriappa Udiyanda Muthanna <nitinkaveriappa@gmail.com>
2018-10-31 11:43:58 +0530

    Init

--- demo/src/App.js
+++ demo/src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
   }
 }
 
