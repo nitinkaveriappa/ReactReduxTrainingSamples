e22d8201ad3777aaa03e508f1eeadc7a70b2b608
Nitin Kaveriappa <Nitin.Kaveriappa@neviton.com>
2018-11-02 18:00:49 +0530

    Update

---
+++ jestenzyme-jotto/src/reducers/successReducer.js
@@ -0,0 +1,9 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {obeject} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+export default (state, action) => {
+  return null;
+};
